e93cf7c44b3344bcd826b60897c2d1a3963417a7
Yamanist <yaman26@proton.me>
2026-09-15 22:33:26 +0300

    comments updated

--- worker.js
+++ worker.js
@@ -130,7 +130,7 @@ export default {
       });
 
       if (!discordResponse.ok) {
-// get the error text so we can see what is wrong
+// grab the error text just so we can figure out what went wrong here
         const text = await discordResponse.text();
         console.log("Discord error:", text);
         return json(502, { error: 'Discord webhook failed.', detail: text }, corsHeaders);
